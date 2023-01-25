4255511c5e6cee2ceab9816a22149e2d950125ea
AbielKiflu <abiel.kiflu.weld@gmail.com>
2023-01-26 00:34:12 +0100

    setup files and folders

---
+++ src/components/Navigation.vue
@@ -0,0 +1,28 @@
+<template>
+<header>
+    <nav>
+        <div class="brand">
+            <router-link class="link" :to="{name:'App'}">Netflix</router-link>
+        </div>
+         <ul>
+            <router-link class="link" to="#"> Home</router-link>
+            <router-link class="link" to="#"> Tv Shows</router-link>
+            <router-link class="link" to="#"> Movies</router-link>
+            <router-link class="link" to="#"> My List</router-link>
+         </ul>
+
+    </nav>
+</header>
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
