7df825f678c57e6f2bd49c6e0f6f2581b0f93ba8
AbielKiflu <abiel.kiflu.weld@gmail.com>
2023-02-11 02:27:19 +0100

    loginstate firebase

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,11 @@
 import {createRouter,createWebHistory} from 'vue-router';
- 
  import WelcomeView from "@/views/WelcomeView.vue";
  import HomeView from "@/views/HomeView.vue";
  import MoviesView from "@/views/MoviesView.vue";
  import MyListView from "@/views/MyListView.vue";
  import TvShowView from "@/views/TvShowView.vue";
  import LoginView from "@/views/LoginView.vue";
- 
+ import {getAuth,onAuthStateChanged} from "firebase/auth";
 
  const router = createRouter({
 
@@ -30,6 +29,9 @@ import {createRouter,createWebHistory} from 'vue-router';
       path: '/home',
       name: 'home',
       component: HomeView,
+      meta:{
+        requiresAuth: true,
+      }
 
     },
 
@@ -37,26 +39,59 @@ import {createRouter,createWebHistory} from 'vue-router';
       path: '/movies',
       name: 'movies',
       component: MoviesView,
-
+      meta:{
+        requiresAuth: true,
+      }
     },
 
     {
       path: '/lists',
       name: 'mylist',
       component: MyListView,
-
+      meta:{
+        requiresAuth: true,
+      }
     },
 
     {
       path: '/shows',
       name: 'tvshow',
       component: TvShowView,
-
+      meta:{
+        requiresAuth: true,
+      }
     },
   
   ],
 
  });
+
+ const getCurrentUser = () =>{
+  return new Promise((resolve, reject) =>{
+    const removeListener=onAuthStateChanged(
+      getAuth(),
+      (user)=>{
+        removeListener();
+        resolve(user);
+      },
+      reject
+    )
+  });
+ }
+
+ router.beforeEach( async (to,from,next)=>{
+   if(to.matched.some((record)=>record.meta.requiresAuth)){
+    if(await getCurrentUser()){
+      next();
+    }else{
+      next("/");
+    }
+   }else{
+    next();
+   }
+
+  }
+ );
  
 
 export default router;
