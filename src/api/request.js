efa28b175eb7458b88f38b84319caf680bbe0a01
AbielKiflu <abiel.kiflu.weld@gmail.com>
2023-02-13 00:05:39 +0100

    genre api

--- src/api/request.js
+++ src/api/request.js
@@ -9,7 +9,14 @@ const requests={
     moviesUpcoming:url+"/movie/upcoming?api_key="+apiKey+"&language=en-US",
     moviesLatest:url+"/movie/latest?api_key="+apiKey+"&language=en-US",
     moviesTopRated:url+"/movie/top_rated?api_key="+apiKey+"&language=en-US",
+
+    genreMovie:url+"/genre/movie/list?api_key="+apiKey+"&language=en-US",
+    genreTv:url+"/genre/tv/list?api_key="+apiKey+"&language=en-US",
+
     }
+
+
+    //http://api.themoviedb.org/3/genre/movie/list?api_key=585a6beaf8e04a94871f1e88f810bd83
 
 
 export default requests;
